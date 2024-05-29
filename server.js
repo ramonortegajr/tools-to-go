2ffa955a5c7d0e840ab590cf441b5f0145331dae
ramram <ramonortegajr1997@gmail.com>
2024-05-29 10:30:22 +0800

    feat: added new tools and change design to 3d

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
-const PORT = process.env.PORT || 2000;
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
+});
+
+app.get('/home', (req, res) => {
+    res.render('home');
 });
 
 app.post('/get-video-info', async (req, res) => {
